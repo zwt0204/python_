dfe5f878623922d4126b64a8f5ad09e8111c7acd
zhangweitao <zhang.weitao@chinaredstar.cn>
2020-07-02 16:41:26 +0800

    'fasttext'

--- word_embedding/fast_test.py
+++ word_embedding/fast_test.py
@@ -8,6 +8,7 @@
 """
 import fasttext
 
+# 吃 味精 怎么 了
 # model = fasttext.train_unsupervised('../data/test.txt', model='skipgram')
 # model.save_model('../data/test.bin')
 # print(model.words)
@@ -15,10 +16,12 @@ import fasttext
 
 # fasttext.load_model()
 
+# 吃 味精 怎么 了   __label__1
 # model = fasttext.train_supervised('../data/train.txt')
 # print(model.labels)
 # model.save_model('../data/fasttext.model.bin')
 
+# 吃 味精 怎么 了   __label__1
 classifier = fasttext.load_model("../data/fasttext.model.bin")
 
 result = classifier.test("../data/train.txt")
